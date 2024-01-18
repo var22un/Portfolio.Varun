294accdcde8824e8557fe901bc1cf6f91b7ec5d2
Varun <155068775+var22un@users.noreply.github.com>
2024-01-18 16:54:05 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#show").hover(function(){
+        $("#ho_v").slideToggle(1000,function(){
+            $("#ho_v").delay(5000);  
+        });
+        
+    });
+   });
+   
+   $(document).ready(function(){
+       $("#web_dev").hover(function(){
+           $("#web_pic").slideToggle(500);
+       });
+   });
+$(document).ready(function(){
+   $("#serv_box").hover(function(){
+       $("#heading1").slideToggle(500);
+   });
+});
+
+$(document).ready(function(){
+   $("#serv_box2").hover(function(){
+       $("#heading2").slideToggle(500);
+   });
+});
+
+$(document).ready(function(){
+   $("#serv_box3").hover(function(){
+       $("#heading3").slideToggle(500);
+   });
+});
+
+$(document).ready(function(){
+   $("#serv_box4").hover(function(){
+       $("#heading4").slideToggle(500);
+   });
+});
+
+
